31caf9b7ef4e886a44add5bb59fd7dba78d08c3b
YevgeniyaK <ekopachevskaya@gmail.com>
2017-08-06 12:53:50 +0200

    Выделили вспомогательный класс для управления сессиями.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact(Contact(firstname="Firstname", middlename="Middlename", lastname="Lastname",
                          nickname="Nickname", title="Mr", company="Company", address="Address line",
                          phone_home="+74951234567", phone_mobile="+79031234567", phone_work="+74961234567",
@@ -22,6 +22,6 @@ def test_add_contact(app):
                          birthday_year="1990", anniversary_day="18", anniversary_month="10",
                          anniversary_year="2010", secondary_address="secondary address",
                          home_address="home address", notes="notes"))
-    app.logout()
+    app.session.logout()
 
 
