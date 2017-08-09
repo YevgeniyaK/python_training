d42dd62ea4d9b716b7b8951535824cbb80cde16b
YevgeniyaK <ekopachevskaya@gmail.com>
2017-08-09 09:09:47 +0200

    Создали тест для изменения группы

--- fixture/group.py
+++ fixture/group.py
@@ -46,3 +46,22 @@ class GroupHelper:
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+
+    '''
+     Модификация группы
+    '''
+    def change_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
