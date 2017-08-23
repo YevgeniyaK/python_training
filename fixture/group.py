18b13358171d1d5742d1c08cffe73c5cd62b2783
YevgeniyaK <ekopachevskaya@gmail.com>
2017-08-23 21:01:13 +0200

    Реализовали проверки в тестах для групп

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,4 @@
+from model.group import Group
 
 class GroupHelper:
 
@@ -100,3 +101,17 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    '''
+    Получение списка групп
+    '''
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name = text, id = id))
+        return groups
